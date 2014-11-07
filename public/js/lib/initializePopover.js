b44cb6b12fc52c4ed3d2322d8bc38f45a119badf
Nicholas Wetmore <n.wetmore@contentbloom.com>
2014-11-07 15:57:16 -0400

    Updates to the style guide to help sync-up the designs with the front end code.

--- public/js/lib/initializePopover.js
+++
@@ -1,9 +0,0 @@
-/*
- * Starts up the Bootstrap popover JavaScript.
- */
-
-$('.js-question-help').popover({
-  template:'<div class="popover" role="tooltip"><p class="popover-content"></p></div>',
-  animation: false
-});
-
